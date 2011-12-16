d88ea477c26c41d19554e21e1064f0e5d8741744
Igor Afonov <afonov@gmail.com>
2011-12-16 18:13:46 +0200

    Oops, no tabs.

--- cosmonaut.c
+++ cosmonaut.c
@@ -15,82 +15,82 @@
 #define MAX_DATA_SIZE 5000
 
 void reset_hints(struct addrinfo *hints) {
-	memset(hints, 0, sizeof *hints);
-	hints->ai_family   = AF_UNSPEC;      // v4/v6
-	hints->ai_socktype = SOCK_STREAM;    // TCP stream sockets
-	hints->ai_flags    = AI_PASSIVE;     // fill in my IP for me
+  memset(hints, 0, sizeof *hints);
+  hints->ai_family   = AF_UNSPEC;      // v4/v6
+  hints->ai_socktype = SOCK_STREAM;    // TCP stream sockets
+  hints->ai_flags    = AI_PASSIVE;     // fill in my IP for me
 }
 
 void *get_in_addr(struct sockaddr *sa) {
-	return &(((struct sockaddr_in*)sa)->sin_addr);
+  return &(((struct sockaddr_in*)sa)->sin_addr);
 }
 
 int bind_server() {
-	int status;
-	int server_socket;
-	struct sockaddr_storage; 
-	struct addrinfo hints;
-	struct addrinfo *servinfo;
+  int status;
+  int server_socket;
+  struct sockaddr_storage;
+  struct addrinfo hints;
+  struct addrinfo *servinfo;
 
-	reset_hints(&hints);
+  reset_hints(&hints);
 
-	if ((status = getaddrinfo(NULL, SERVER_PORT, &hints, &servinfo)) != 0) {
-		print_log("error: %s\n", gai_strerror(status));
-		exit(1);
-	}
+  if ((status = getaddrinfo(NULL, SERVER_PORT, &hints, &servinfo)) != 0) {
+    print_log("error: %s\n", gai_strerror(status));
+    exit(1);
+  }
 
-	server_socket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+  server_socket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
 
-	if ((status = bind(server_socket, servinfo->ai_addr, servinfo->ai_addrlen)) != 0) {
-		print_log("error: %s\n", gai_strerror(status));
-		exit(1);
-	}
+  if ((status = bind(server_socket, servinfo->ai_addr, servinfo->ai_addrlen)) != 0) {
+    print_log("error: %s\n", gai_strerror(status));
+    exit(1);
+  }
 
-	if ((status = listen(server_socket, 20)) != 0) {
-		print_log("error: %s\n", gai_strerror(status));
-		exit(1);
-	}
+  if ((status = listen(server_socket, 20)) != 0) {
+    print_log("error: %s\n", gai_strerror(status));
+    exit(1);
+  }
 
-	print_log("started on port %s", SERVER_PORT);
+  print_log("started on port %s", SERVER_PORT);
 
-	// clean up
-	freeaddrinfo(servinfo);
+  // clean up
+  freeaddrinfo(servinfo);
 
-	return server_socket;
+  return server_socket;
 }
 
 int main(int argc, char *argv[]) {
-	struct sockaddr their_addr;
-	int new_fd;
-	socklen_t addr_size = sizeof their_addr;
-	setup_signal_listener();
-	int server_socket = bind_server();
-	char s[INET6_ADDRSTRLEN];
-	char buf[MAX_DATA_SIZE];
-	int numbytes = 0;
+  struct sockaddr their_addr;
+  int new_fd;
+  socklen_t addr_size = sizeof their_addr;
+  setup_signal_listener();
+  int server_socket = bind_server();
+  char s[INET6_ADDRSTRLEN];
+  char buf[MAX_DATA_SIZE];
+  int numbytes = 0;
 
-	while(1) {
-		new_fd = accept(server_socket, (struct sockaddr *)&their_addr, &addr_size);
+  while(1) {
+    new_fd = accept(server_socket, (struct sockaddr *)&their_addr, &addr_size);
 
-		inet_ntop(AF_INET, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-		print_log("server: got connection from %s", s);
+    inet_ntop(AF_INET, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+    print_log("server: got connection from %s", s);
 
-		if (fork()) {
-			close(server_socket);
+    if (fork()) {
+      close(server_socket);
 
-			numbytes = recv(new_fd, &buf, MAX_DATA_SIZE, 0);
-			buf[numbytes] = '\0';
-			print_log("%s", buf);
+      numbytes = recv(new_fd, &buf, MAX_DATA_SIZE, 0);
+      buf[numbytes] = '\0';
+      print_log("%s", buf);
 
-			if (send(new_fd, "It works", 8, 0) == -1) {
-				print_log("error sending data");
-			}
-			close(new_fd);
-			exit(0);
-		}
+      if (send(new_fd, "It works", 8, 0) == -1) {
+        print_log("error sending data");
+      }
+      close(new_fd);
+      exit(0);
+    }
 
-		shutdown(new_fd, SHUT_RDWR);
-	}
+    shutdown(new_fd, SHUT_RDWR);
+  }
 
-	return 0;
+  return 0;
 }
--- util/signals.c
+++ util/signals.c
@@ -1,16 +1,16 @@
 #include <signal.h>
 
 void sigchld_handler(int signal) {
-	while(waitpid(-1, NULL, WNOHANG) > 0);
+  while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
 void setup_signal_listener() {
-	struct sigaction sa;
-	sa.sa_handler = sigchld_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
+  struct sigaction sa;
+  sa.sa_handler = sigchld_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
 
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		exit(1);
-	}
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    exit(1);
+  }
 }
