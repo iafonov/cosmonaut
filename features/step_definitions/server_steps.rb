36299a4a2dc6b8ff8303a04b4f5e2e7cd622cfd2
Igor Afonov <afonov@gmail.com>
2012-01-08 16:17:26 +0200

    Fixed feature.

--- features/step_definitions/server_steps.rb
+++ features/step_definitions/server_steps.rb
@@ -7,7 +7,7 @@ When /^I start cosmonaut and supply "([^"]*)" as config file$/ do |conf_file_nam
 end
 
 Then /^cosmonaut should pickup "([^"]*)" as config file$/ do |conf_file_name|
-  COSMONAUT.std_err.should include("loading config from #{fixture_file_path(conf_file_name)}")
+  COSMONAUT.std_err.should include("#{fixture_file_path(conf_file_name)}")
 end
 
 Then /^cosmonaut should listen port (\d+)$/ do |port|
