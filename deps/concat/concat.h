33f6dc6973c3af522a1e132754e67c906b4478c9
Igor Afonov <afonov@gmail.com>
2012-01-13 23:30:16 +0200

    Removed temporary utility files.

--- deps/concat/concat.h
+++
@@ -1,7 +0,0 @@
-#ifndef _concat_h
-#define _concat_h
-
-char * concat(const char *first, ...);
-size_t concat_len(const char *first, ...);
-
-#endif
--- features/request.feature
+++
@@ -1,6 +0,0 @@
-@javascript
-Feature: Requesting cosmonaut
-
-Scenario: Home page
-  When I go to /
-  Then I should see "It works!"
